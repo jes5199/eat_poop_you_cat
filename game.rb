75c994f37aca17ba0e9d207cc3dd1ac8db6564ed
Jesse Wolfe <jes5199@gmail.com>
2011-10-17 17:56:43 -0700

    sketch api

--- game.rb
+++ game.rb
@@ -3,3 +3,30 @@ require 'sinatra'
 get '/hi' do
   "Hello World!"
 end
+
+post '/games/?' do
+  # returns a new game ID
+  Game.new
+end
+
+get '/games/?' do
+  # returns a list of games
+  Game.all
+end
+
+post '/games/:game_id/?' do
+  # returns a player ID
+  Game.
+end
+
+get '/games/:game_id/:player_id/?' do
+  # returns your turn, if it's your turn
+end
+
+post '/games/:game_id/:player_id/drawing/?' do
+  # plays a drawing
+end
+
+post '/games/:game_id/:player_id/sentence/?' do
+  # plays a sentence
+end
